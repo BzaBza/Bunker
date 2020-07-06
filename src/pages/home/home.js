2365941688b17c479ac82d3f0dac5fdfb44bd574
BzaBza <mailarthura@gmail.com>
2020-07-06 19:06:26 +0300

    Add firebase

---
+++ src/pages/home/home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>Hi, this is NOT bunker game ;)</h1>
+      <div><button>Generate card</button></div>
+    </div>
+  );
+}
+
+export default Home;

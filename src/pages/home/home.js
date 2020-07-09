e56479fa72b2e357c9ff8f8ea311d07debfba20c
BzaBza <mailarthura@gmail.com>
2020-07-09 22:06:20 +0300

    Add subscribe on sessions

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -4,11 +4,12 @@ import {db} from "../../firebase";
 class Home extends React.Component {
 
     componentWillMount() {
+        this.getMarkers()
     }
 
     constructor(props) {
         super(props);
-        this.state = {sName: '',sessionList: ["List Item 1", "List Item 2", "List Item 3"]};
+        this.state = {sName: '', sessionList: ["List Item 1", "List Item 2", "List Item 3"]};
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -26,7 +27,7 @@ class Home extends React.Component {
     createSession(data) {
 
         db.collection('sessions').doc(data).set({
-            name:data,
+            name: data,
             id: this.state.sessionList.length,
             state: "Waiting",
             players: 0
@@ -35,15 +36,15 @@ class Home extends React.Component {
         console.log("Started");
     }
 
-    deleteSession(data){
+    deleteSession(data) {
         db.collection('sessions').doc(data).delete();
         console.log("Deleted");
     }
 
-    async deleteAllSessions(){
+    async deleteAllSessions() {
         //this.getMarkers() ;
-        for(const item of this.state.sessionList){
-            console.log("Deleted",item)
+        for (const item of this.state.sessionList) {
+            console.log("Deleted", item)
             this.deleteSession(item);
         }
 
@@ -51,52 +52,38 @@ class Home extends React.Component {
 
 
     async getMarkers() {
-        this.state.sessionList=[];
-        let listItems=[]
-        const events = await db.collection('sessions')
-        events.get().then((querySnapshot) => {
-            const tempDoc = querySnapshot.docs.map((doc) => {
+         db.collection("sessions")
+            .onSnapshot((snapshot) => {
+                console.log(snapshot.docs.map(doc => doc.data()))
 
-                return { id: doc.id, ...doc.data() }
-            })
-            for(const temp of tempDoc){
-                this.state.sessionList.push(temp.name)
-                console.log(temp.name)}
-            console.log("this.state.sessionList ",this.state.sessionList)
-
-
-        })
-        return listItems;
-    };
-
-
+                this.setState(()=> this.state.sessionList = snapshot.docs.map(doc => doc.data()))
+            });
+    }
 
 
     render() {
-        //console.log("this.state.sessionList ",this.state.sessionList)
-
-        //this.getMarkers();
-
         return (
             <div>
                 <h1>Hi, this is NOT bunker game ;)</h1>
                 <label>Session Name:</label>
                 <input type="text" value={this.state.value} onChange={this.handleChange}/>
-                <div><button onClick={()=>this.createSession(this.state.sName)}>Create session</button></div>
-                <div><button onClick={()=>this.deleteSession(this.state.sName)}>End session</button></div>
-                <div><button onClick={()=>this.getMarkers()}>List</button></div>
-                <div><button onClick={()=>this.deleteAllSessions()}>Clear</button></div>
+                <div>
+                    <button onClick={() => this.createSession(this.state.sName)}>Create session</button>
+                </div>
+                <div>
+                    <button onClick={() => this.deleteSession(this.state.sName)}>End session</button>
+                </div>
+                <div>
+                    <button onClick={() => this.deleteAllSessions()}>Clear</button>
+                </div>
 
-                <React.Fragment>
-                    <ul className="list-group">
-                        {this.state.sessionList.map(listitem => (
-                            <li className="list-group-item list-group-item-primary">
-                                {listitem}
-                            </li>
-                        ))}
-                    </ul>
-                </React.Fragment>
-
+                <ul className="list-group">
+                    {this.state.sessionList.map(listitem => (
+                        <li className="list-group-item list-group-item-primary">
+                            {listitem['name']}
+                        </li>
+                    ))}
+                </ul>
             </div>
         );
     }
